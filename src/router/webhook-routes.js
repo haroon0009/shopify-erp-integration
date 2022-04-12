e82ac9bdcf4baaa8a39a663942adb75bb72fd228
haroon <haroonshahzad203@gmail.com>
2022-04-12 17:16:18 +0500

    webhook route prevent from calling

--- src/router/webhook-routes.js
+++ src/router/webhook-routes.js
@@ -1,59 +1,59 @@
-import express from "express";
-import { validateWebhook } from "../middleware/index.js";
-import { uninstallShopHandler, webhookHandler } from "../webhook/index.js";
+import express from 'express';
+import { validateWebhook } from '../middleware/index.js';
+import { uninstallShopHandler, webhookHandler } from '../webhook/index.js';
 
 export const webhookRoutes = new express.Router();
 
-webhookRoutes.post("/uninstall-shop", validateWebhook, (req, res) => {
+webhookRoutes.post('/uninstall-shop', validateWebhook, (req, res) => {
   console.log(`${req.shop_name} uninstalled the app`);
   uninstallShopHandler(req.body);
-  res.status(200).send("OK");
+  res.status(200).send('OK');
 });
 
-webhookRoutes.post("/orders-cancelled", validateWebhook, (req, res) => {
-  webhookHandler({
-    path: "/orders-cancelled",
-    body: req.body,
-  });
-  res.status(200).send("OK");
+webhookRoutes.post('/orders-cancelled', validateWebhook, (req, res) => {
+  // webhookHandler({
+  //   path: "/orders-cancelled",
+  //   body: req.body,
+  // });
+  res.status(200).send('OK');
 });
 
-webhookRoutes.post("/orders-create", validateWebhook, (req, res) => {
-  webhookHandler({
-    path: "/orders-create",
-    body: req.body,
-  });
-  res.status(200).send("OK");
+webhookRoutes.post('/orders-create', validateWebhook, (req, res) => {
+  // webhookHandler({
+  //   path: '/orders-create',
+  //   body: req.body,
+  // });
+  res.status(200).send('OK');
 });
 
-webhookRoutes.post("/orders-fulfilled", validateWebhook, (req, res) => {
+webhookRoutes.post('/orders-fulfilled', validateWebhook, (req, res) => {
   webhookHandler({
-    path: "/OrdersFulfill",
+    path: '/OrdersFulfill',
     body: req.body,
   });
-  res.status(200).send("OK");
+  res.status(200).send('OK');
 });
 
-webhookRoutes.post("/orders-paid", validateWebhook, (req, res) => {
-  webhookHandler({
-    path: "/orders-paid",
-    body: req.body,
-  });
-  res.status(200).send("OK");
+webhookRoutes.post('/orders-paid', validateWebhook, (req, res) => {
+  // webhookHandler({
+  //   path: "/orders-paid",
+  //   body: req.body,
+  // });
+  res.status(200).send('OK');
 });
 
-webhookRoutes.post("/orders-updated", validateWebhook, (req, res) => {
-  webhookHandler({
-    path: "/orders-updated",
-    body: req.body,
-  });
-  res.status(200).send("OK");
+webhookRoutes.post('/orders-updated', validateWebhook, (req, res) => {
+  // webhookHandler({
+  //   path: "/orders-updated",
+  //   body: req.body,
+  // });
+  res.status(200).send('OK');
 });
 
-webhookRoutes.post("/customer-create", validateWebhook, (req, res) => {
-  webhookHandler({
-    path: "/customer-create",
-    body: req.body,
-  });
-  res.status(200).send("OK");
+webhookRoutes.post('/customer-create', validateWebhook, (req, res) => {
+  // webhookHandler({
+  //   path: '/customer-create',
+  //   body: req.body,
+  // });
+  res.status(200).send('OK');
 });
